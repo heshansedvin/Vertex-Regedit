5655eea811c06bd167a78e3ead987667ea416f8a
Heshan S Edvin <heshansankalpa2004@gmail.com>
2024-07-22 20:59:16 +0530

    Update scripts.js

--- web/scripts.js
+++ web/scripts.js
@@ -6,8 +6,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const message = document.getElementById('message');
 
     if (username === '11' && password === '1') {
-        message.textContent = 'Login successful!';
-        message.style.color = 'green';
+        window.location.href = 'download.html';
     } else {
         message.textContent = 'Invalid username or password!';
     }
